90e73ec1928ea715ab52eec72e48b55d2614910b
Григорий Кузеванов <grkuzevanov@gmail.com>
2024-11-13 14:21:33 +0500

    allow all hosts in settings.

--- wm_practice/wm_practice/settings.py
+++ wm_practice/wm_practice/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     'dogs',
-    'rest_framwork',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
