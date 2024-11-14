5a158cbf7ad9cd6166cd1d6f5e694c603238ab29
Григорий Кузеванов <grkuzevanov@gmail.com>
2024-11-14 17:03:14 +0500

    add dog list view.

--- wm_practice/dogs/controllers.py
+++ wm_practice/dogs/controllers.py
@@ -1,3 +1,4 @@
+from django.db.models.query import QuerySet
 from django.http import Http404
 from rest_framework import status
 from rest_framework.response import Response
@@ -47,7 +48,7 @@ class DogDetail(APIView):
 
     def put(self, request, pk: int) -> Response:
         """
-        Изменяет данные о собаке по переданному идентификатору.
+        Изменить данные о собаке по переданному идентификатору.
 
         Args:
             pk (int): идентификатор собаки
@@ -64,21 +65,54 @@ class DogDetail(APIView):
 
     def delete(self, request, pk: int) -> Response:
         """
-        Удаляет данные о собаке по переданному идентификатору.
+        Удалить данные о собаке по переданному идентификатору.
 
         Args:
             pk (int): идентификатор собаки
         """
-        
+
         dog_model = self.get_object(pk)
         dog_model.delete()
-        
+
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class DogList(APIView):
-    def get(self):
-        pass
+    """
+    Класс для обработки запросов на получение списка собак
+    и добавления данных новой собаки.
 
-    def post(self):
-        pass
+    - GET: Получить список данных о всех собаках
+    - POST: Добавить данные о новой собаке
+    """
+
+    @staticmethod
+    def get_queryset() -> QuerySet[Dog]:
+        """
+        Получить список всех записей собак из БД.
+        """
+
+        return Dog.objects.all()
+
+    def get(self, request) -> Response:
+        """
+        Получить список всех собак из БД.
+        """
+
+        dog_models = self.get_queryset()
+        serializer = DogSerializer(dog_models, many=True)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request) -> Response:
+        """
+        Добавить данные о новой собаке с переданными параметрами.
+        """
+
+        dog_serializer = DogSerializer(data=request.data)
+
+        if dog_serializer.is_valid():
+            dog_serializer.save()
+            return Response(dog_serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(dog_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
