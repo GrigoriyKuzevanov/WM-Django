fbf60dbc5f38ae5c7b6c2ab8ceb9f19023567444
Григорий Кузеванов <grkuzevanov@gmail.com>
2024-11-14 17:03:22 +0500

    add url for dog list view.

--- wm_practice/dogs/urls.py
+++ wm_practice/dogs/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .controllers import DogDetail
+from .controllers import DogDetail, DogList
 
 
 urlpatterns = [
     path("dogs/<int:pk>/", DogDetail.as_view(), name="dog-detail"),
+    path("dogs/", DogList.as_view(), name="dog_list"),
 ]
